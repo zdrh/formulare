e4a46a457dff199bd29a09f85c2354310132ba32
Zdenek Hrdina <hrdina.zdenek@gmail.com>
2026-05-13 15:34:05 +0200

    drobnosti

--- app/Views/main/index.php
+++ app/Views/main/index.php
@@ -9,5 +9,7 @@
 <h2><a href="<?= base_url('form-basic-undelete') ?>">Rozšířená základní varianta</a></h2>
 <p>Základní varianta vylepšená o možnost undelete, tede o vrácení smzaných položek.</p>
 <h2><a href="<?= base_url('form-helper') ?>">Varianta s helperem</a></h2>
-<p>Vytvořil jsem vlastní helper pro rychlejší psaní inputů, modalů  dropdownů. TEn jsem uložil do složky helpers a následně povolil v BaseControlleru. Nyní budu jendotlivé formuláře ve view generovat pohodlněji a rychleji.</p>
+<p>Vytvořil jsem vlastní helper pro rychlejší psaní inputů, modalů  dropdownů. Ten jsem uložil do složky helpers a následně povolil v BaseControlleru. Nyní budu jendotlivé formuláře ve view generovat pohodlněji a rychleji.</p>
+<h2><a href="<?= base_url('form-helper') ?>">Varianta s alerty</a></h2>
+<p>Vytvořil jsem knihovnu, která slouží jako service pro alerty (hlášky o přidání, eidtaci nebo smazání). Hlášky jsou uložené ve složce jazykových překladů, takže je pak jednoduché hlášky udělat vícejazyčne.</p>
 <?= $this->endSection() ?>
